ddad2cbba8e1f9494bbcbf75eb2e6ef98c4a6473
BhupalReddySama262318 <bhupalreddysama18@gmail.com>
2024-08-27 18:31:43 +0530

    add files

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Python Developer", "MySQL Developer"],
+    typeSpeed: 150,
+    // backSpeed: 150,
+    backDelay: 500,
+    loop: true
+});
+
+
